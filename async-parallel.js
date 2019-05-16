c8348c0afa91f8931b727ab9f2fe4e23ac0040d5
joycebansode2 <Joyce.Bansode@harbingergroup.com>
2019-05-16 15:14:29 +0530

    difference in async series and parallel

--- async-parallel.js
+++ async-parallel.js
@@ -4,12 +4,6 @@ const http = require('http');
 const async = require('async');
 const access_key = '47173ef6e1b30e8b4dcc288c0f5d176a';
 
-'use strict'
-
-const http = require('http');
-const async = require('async');
-const access_key = '47173ef6e1b30e8b4dcc288c0f5d176a';
-
 let symbolValueMapping = () =>{
     let base = 'AMD';
     let symbols = 'INR,USD,ALL,EUR,YER';
